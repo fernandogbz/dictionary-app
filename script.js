edd285255bbeacd7e19f1e88c0f991edc44d8c90
Fernando Jimenez <fgjv06@gmail.com>
2023-02-26 03:44:38 -0300

    JS - .catch added

--- script.js
+++ script.js
@@ -27,7 +27,10 @@ btn.addEventListener("click", () => {
 					${data[0].meanings[0].definitions[0].example || ""}
 				</p>`;
       sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
-    });
+    })
+    .catch(() => {
+      result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+    })
 });
 
 const playSound = () => {
