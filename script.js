ee9d36003d26f5919108e485380473e711e9e6b2
Fernando Jimenez <fgjv06@gmail.com>
2023-02-26 03:08:20 -0300

    JS - audio added when press sound button

--- script.js
+++ script.js
@@ -26,6 +26,10 @@ btn.addEventListener("click", () => {
 				<p class="word-example">
 					${data[0].meanings[0].definitions[0].example || ""}
 				</p>`;
-        sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`)
+      sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
     });
-})
+});
+
+function playSound() {
+  sound.play();
+}
